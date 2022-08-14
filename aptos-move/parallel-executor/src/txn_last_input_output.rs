3717f0042b235cc69a4424f658e50d2ab3fdae4c
Rati Gelashvili <gelash@users.noreply.github.com>
2022-08-13 18:01:09 -0700

    Revert "don't preemptively return threads upon detecting possible module publishing race (#2876)"
    This reverts commit 07693def7ac5f551f7ede54adb17c7e20f28eee7.

--- aptos-move/parallel-executor/src/txn_last_input_output.rs
+++ aptos-move/parallel-executor/src/txn_last_input_output.rs
@@ -6,17 +6,12 @@ use crate::{
     scheduler::{Incarnation, TxnIndex, Version},
     task::{ExecutionStatus, ModulePath, Transaction, TransactionOutput},
 };
+use anyhow::{bail, Result};
 use aptos_types::access_path::AccessPath;
 use arc_swap::ArcSwapOption;
 use crossbeam::utils::CachePadded;
 use dashmap::DashSet;
-use std::{
-    collections::HashSet,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
-};
+use std::{collections::HashSet, sync::Arc};
 
 type TxnInput<K> = Vec<ReadDescriptor<K>>;
 type TxnOutput<T, E> = ExecutionStatus<T, Error<E>>;
@@ -83,8 +78,6 @@ pub struct TxnLastInputOutput<K, T, E> {
     // Move-VM loader cache - see 'record' function comment for more information.
     module_writes: DashSet<AccessPath>,
     module_reads: DashSet<AccessPath>,
-
-    module_read_write_intersection: AtomicBool,
 }
 
 impl<K: ModulePath, T: TransactionOutput, E: Send + Clone> TxnLastInputOutput<K, T, E> {
@@ -98,7 +91,6 @@ impl<K: ModulePath, T: TransactionOutput, E: Send + Clone> TxnLastInputOutput<K,
                 .collect(),
             module_writes: DashSet::new(),
             module_reads: DashSet::new(),
-            module_read_write_intersection: AtomicBool::new(false),
         }
     }
 
@@ -106,16 +98,16 @@ impl<K: ModulePath, T: TransactionOutput, E: Send + Clone> TxnLastInputOutput<K,
         paths: Vec<AccessPath>,
         set_to_append: &DashSet<AccessPath>,
         set_to_check: &DashSet<AccessPath>,
-    ) -> bool {
+    ) -> Result<()> {
         for path in paths {
             // Standard flags, first show, then look.
             set_to_append.insert(path.clone());
 
             if set_to_check.contains(&path) {
-                return true;
+                bail!("Module published and also read");
             }
         }
-        false
+        Ok(())
     }
 
     /// Returns an error if a module path that was read was previously written to, and vice versa.
@@ -135,7 +127,7 @@ impl<K: ModulePath, T: TransactionOutput, E: Send + Clone> TxnLastInputOutput<K,
         txn_idx: TxnIndex,
         input: Vec<ReadDescriptor<K>>,
         output: ExecutionStatus<T, Error<E>>,
-    ) {
+    ) -> Result<()> {
         let read_modules: Vec<AccessPath> =
             input.iter().filter_map(|desc| desc.module_path()).collect();
         let written_modules: Vec<AccessPath> = match &output {
@@ -147,24 +139,12 @@ impl<K: ModulePath, T: TransactionOutput, E: Send + Clone> TxnLastInputOutput<K,
             ExecutionStatus::Abort(_) => Vec::new(),
         };
 
-        if !self.module_read_write_intersection.load(Ordering::Relaxed) {
-            self.module_read_write_intersection.store(
-                Self::append_and_check(read_modules, &self.module_reads, &self.module_writes)
-                    || Self::append_and_check(
-                        written_modules,
-                        &self.module_writes,
-                        &self.module_reads,
-                    ),
-                Ordering::Release,
-            );
-        }
+        Self::append_and_check(read_modules, &self.module_reads, &self.module_writes)?;
+        Self::append_and_check(written_modules, &self.module_writes, &self.module_reads)?;
 
         self.inputs[txn_idx].store(Some(Arc::new(input)));
         self.outputs[txn_idx].store(Some(Arc::new(output)));
-    }
-
-    pub fn module_publishing_may_race(&self) -> bool {
-        self.module_read_write_intersection.load(Ordering::Acquire)
+        Ok(())
     }
 
     pub fn read_set(&self, txn_idx: TxnIndex) -> Option<Arc<Vec<ReadDescriptor<K>>>> {
